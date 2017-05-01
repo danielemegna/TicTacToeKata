024846895d806160926767ff03b8f0aee87db6ae
Daniele Megna <megna.dany@gmail.com>
2017-05-01 11:26:22 +0200

    Added start_game return value assertions

--- elixir/game/test/game__golden_master__test.exs
+++ elixir/game/test/game__golden_master__test.exs
@@ -12,21 +12,28 @@ defmodule Game_GoldenMaster_Test do
   test "fast game over golden master" do
     assert game("0\n1\n3\n") ==
       "\n--------------\n\n  0   1   2\n ===+===+=== \n  3   4   5\n ===+===+=== \n  6   7   8\n\n--------------\n\nEnter [0-8]\n--------------\n\n  X   1   2\n ===+===+=== \n  3   4   5\n ===+===+=== \n  6   7   8\n\n--------------\n\n\n--------------\n\n  X   1   2\n ===+===+=== \n  3   O   5\n ===+===+=== \n  6   7   8\n\n--------------\n\nEnter [0-8]\n--------------\n\n  X   X   2\n ===+===+=== \n  3   O   5\n ===+===+=== \n  6   7   8\n\n--------------\n\n\n--------------\n\n  X   X   O\n ===+===+=== \n  3   O   5\n ===+===+=== \n  6   7   8\n\n--------------\n\nEnter [0-8]\n--------------\n\n  X   X   O\n ===+===+=== \n  X   O   5\n ===+===+=== \n  6   7   8\n\n--------------\n\n\n--------------\n\n  X   X   O\n ===+===+=== \n  X   O   5\n ===+===+=== \n  O   7   8\n\n--------------\n\nGame Over"
+
+    assert_received {:game_result, :ok}
   end
 
   test "tie game golden master" do
     assert game("0\n6\n5\n1\n7\n") ==
     "\n--------------\n\n  0   1   2\n ===+===+=== \n  3   4   5\n ===+===+=== \n  6   7   8\n\n--------------\n\nEnter [0-8]\n--------------\n\n  X   1   2\n ===+===+=== \n  3   4   5\n ===+===+=== \n  6   7   8\n\n--------------\n\n\n--------------\n\n  X   1   2\n ===+===+=== \n  3   O   5\n ===+===+=== \n  6   7   8\n\n--------------\n\nEnter [0-8]\n--------------\n\n  X   1   2\n ===+===+=== \n  3   O   5\n ===+===+=== \n  X   7   8\n\n--------------\n\n\n--------------\n\n  X   1   2\n ===+===+=== \n  O   O   5\n ===+===+=== \n  X   7   8\n\n--------------\n\nEnter [0-8]\n--------------\n\n  X   1   2\n ===+===+=== \n  O   O   X\n ===+===+=== \n  X   7   8\n\n--------------\n\n\n--------------\n\n  X   1   2\n ===+===+=== \n  O   O   X\n ===+===+=== \n  X   7   O\n\n--------------\n\nEnter [0-8]\n--------------\n\n  X   X   2\n ===+===+=== \n  O   O   X\n ===+===+=== \n  X   7   O\n\n--------------\n\n\n--------------\n\n  X   X   O\n ===+===+=== \n  O   O   X\n ===+===+=== \n  X   7   O\n\n--------------\n\nEnter [0-8]\n--------------\n\n  X   X   O\n ===+===+=== \n  O   O   X\n ===+===+=== \n  X   X   O\n\n--------------\n\nGame Over"
+
+    assert_received {:game_result, :ok}
   end
 
   test "win game golden master" do
     assert game("6\n2\n0\n3\n") ==
     "\n--------------\n\n  0   1   2\n ===+===+=== \n  3   4   5\n ===+===+=== \n  6   7   8\n\n--------------\n\nEnter [0-8]\n--------------\n\n  0   1   2\n ===+===+=== \n  3   4   5\n ===+===+=== \n  X   7   8\n\n--------------\n\n\n--------------\n\n  0   1   2\n ===+===+=== \n  3   O   5\n ===+===+=== \n  X   7   8\n\n--------------\n\nEnter [0-8]\n--------------\n\n  0   1   X\n ===+===+=== \n  3   O   5\n ===+===+=== \n  X   7   8\n\n--------------\n\n\n--------------\n\n  0   1   X\n ===+===+=== \n  3   O   5\n ===+===+=== \n  X   7   O\n\n--------------\n\nEnter [0-8]\n--------------\n\n  X   1   X\n ===+===+=== \n  3   O   5\n ===+===+=== \n  X   7   O\n\n--------------\n\n\n--------------\n\n  X   O   X\n ===+===+=== \n  3   O   5\n ===+===+=== \n  X   7   O\n\n--------------\n\nEnter [0-8]\n--------------\n\n  X   O   X\n ===+===+=== \n  X   O   5\n ===+===+=== \n  X   7   O\n\n--------------\n\nGame Over"
+
+    assert_received {:game_result, :ok}
   end
 
   defp game(input) do
     capture_io([input: input, capture_prompt: false], fn ->
-      Game.start_game
+      game_result = Game.start_game
+      send self(), {:game_result, game_result}
     end)
   end
 
