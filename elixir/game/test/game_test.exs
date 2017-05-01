97aff7e5a3fba3d377010ddb56f0d1928dd58f75
Daniele Megna <megna.dany@gmail.com>
2017-05-01 09:44:43 +0200

    Improved expected board print formatting

--- elixir/game/test/game_test.exs
+++ elixir/game/test/game_test.exs
@@ -6,7 +6,15 @@ defmodule GameTest do
 
   test "print board with an empty list" do
     expected =
-      "\n--------------\n\n        \n ===+===+=== \n        \n ===+===+=== \n        \n\n--------------\n\n"
+      "\n--------------"<>
+      "\n"<>
+      "\n        "<>
+      "\n ===+===+=== "<>
+      "\n        "<>
+      "\n ===+===+=== "<>
+      "\n        "<>
+      "\n"<>
+      "\n--------------\n\n"
 
     assert capture_io(fn ->
       Game.print_board([])
@@ -15,7 +23,15 @@ defmodule GameTest do
 
   test "print an empty board" do
     expected =
-      "\n--------------\n\n  0   1   2\n ===+===+=== \n  3   4   5\n ===+===+=== \n  6   7   8\n\n--------------\n\n"
+      "\n--------------"<>
+      "\n"<>
+      "\n  0   1   2"<>
+      "\n ===+===+=== "<>
+      "\n  3   4   5"<>
+      "\n ===+===+=== "<>
+      "\n  6   7   8"<>
+      "\n"<>
+      "\n--------------\n\n"
 
     assert capture_io(fn ->
       Game.print_board([0, 1, 2, 3, 4, 5, 6, 7, 8])
