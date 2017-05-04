6d5938f60ef8f7173527a99d34400b2cfd783b90
Daniele Megna <daniele.megna@xpeppers.com>
2017-05-04 08:55:18 +0200

    Added Game.tie characterization tests

---
+++ elixir/game/test/game__tie__test.exs
@@ -0,0 +1,35 @@
+defmodule Game_Tie_Test do
+  use ExUnit.Case
+
+  test "tie raise a Protocol.UndefinedError with a null list" do
+    assert_raise Protocol.UndefinedError, fn ->
+      Game.tie(nil)
+    end
+  end
+
+  test "tie returns true with an empty list" do
+    assert Game.tie([]) == true
+  end
+
+  test "tie returns false with an empty board" do
+    assert Game.tie([
+      0,1,2,
+      3,4,5,
+      6,7,8]) == false
+  end
+
+  test "tie returns false when board has free spaces" do
+    assert Game.tie([
+      X,1,O,
+      3,O,5,
+      6,X,8]) == false
+  end
+
+  test "full board real tie returns.... false!?" do
+    assert Game.tie([
+      X,O,X,
+      X,O,X,
+      O,X,O]) == false
+  end
+
+end
