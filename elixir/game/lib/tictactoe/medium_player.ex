40122b30b32c290a9d558054602bf8d3d0db5040
Daniele Megna <megna.dany@gmail.com>
2017-05-07 11:08:53 +0200

    Removed some warning due unused variables

--- elixir/game/lib/tictactoe/medium_player.ex
+++ elixir/game/lib/tictactoe/medium_player.ex
@@ -10,18 +10,18 @@ defmodule TicTacToe.MediumPlayer do
   defp evaluate(board) do
     indexed_board = Enum.with_index(board, 0)
     filtered = Enum.filter(indexed_board, fn({value, _}) -> value != "X" && value != "O" end)
-    available_moves = Enum.map(filtered, fn({value, index}) -> index end)
+    available_moves = Enum.map(filtered, fn({_, index}) -> index end)
     get_best_move(available_moves, board)
   end
 
-  defp get_best_move([first|[]], board) do
+  defp get_best_move([first|[]], _) do
     first
   end
 
   defp get_best_move([first_index|rest], board) do
     cond do
-      game_over(List.update_at(board, first_index, fn(x) -> "O" end)) -> first_index
-      game_over(List.update_at(board, first_index, fn(x) -> "X" end)) -> first_index
+      game_over(List.update_at(board, first_index, fn(_) -> "O" end)) -> first_index
+      game_over(List.update_at(board, first_index, fn(_) -> "X" end)) -> first_index
       true -> get_best_move(rest, board)
     end
   end
