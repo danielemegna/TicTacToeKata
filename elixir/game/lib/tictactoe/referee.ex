73bcd3b84f469f44ed9f20199940f0f742c6e6bc
Daniele Megna <megna.dany@gmail.com>
2017-05-10 08:14:03 +0200

    Integrating Referee with Board struct

--- elixir/game/lib/tictactoe/referee.ex
+++ elixir/game/lib/tictactoe/referee.ex
@@ -1,4 +1,5 @@
 defmodule TicTacToe.Referee do
+  alias TicTacToe.Board
 
   def tie?(board) do
     free_spaces = Enum.any?(board, fn(x) -> x != "X" && x != "O" end)
@@ -6,14 +7,14 @@ defmodule TicTacToe.Referee do
   end
 
   def game_over?(board) do
-    Enum.at(board, 0) == Enum.at(board, 1) && Enum.at(board, 1) == Enum.at(board, 2) ||
-    Enum.at(board, 3) == Enum.at(board, 4) && Enum.at(board, 4) == Enum.at(board, 5) ||
-    Enum.at(board, 6) == Enum.at(board, 7) && Enum.at(board, 7) == Enum.at(board, 8) ||
-    Enum.at(board, 0) == Enum.at(board, 3) && Enum.at(board, 3) == Enum.at(board, 6) ||
-    Enum.at(board, 1) == Enum.at(board, 4) && Enum.at(board, 4) == Enum.at(board, 7) ||
-    Enum.at(board, 2) == Enum.at(board, 5) && Enum.at(board, 5) == Enum.at(board, 8) ||
-    Enum.at(board, 0) == Enum.at(board, 4) && Enum.at(board, 4) == Enum.at(board, 8) ||
-    Enum.at(board, 2) == Enum.at(board, 4) && Enum.at(board, 4) == Enum.at(board, 6)
+    Board.at(board, 0) == Board.at(board, 1) && Board.at(board, 1) == Board.at(board, 2) ||
+    Board.at(board, 3) == Board.at(board, 4) && Board.at(board, 4) == Board.at(board, 5) ||
+    Board.at(board, 6) == Board.at(board, 7) && Board.at(board, 7) == Board.at(board, 8) ||
+    Board.at(board, 0) == Board.at(board, 3) && Board.at(board, 3) == Board.at(board, 6) ||
+    Board.at(board, 1) == Board.at(board, 4) && Board.at(board, 4) == Board.at(board, 7) ||
+    Board.at(board, 2) == Board.at(board, 5) && Board.at(board, 5) == Board.at(board, 8) ||
+    Board.at(board, 0) == Board.at(board, 4) && Board.at(board, 4) == Board.at(board, 8) ||
+    Board.at(board, 2) == Board.at(board, 4) && Board.at(board, 4) == Board.at(board, 6)
   end
 
 end
--- elixir/game/test/referee__game_over__test.exs
+++ elixir/game/test/referee__game_over__test.exs
@@ -1,36 +1,30 @@
 defmodule Referee_GameOver_Test do
   use ExUnit.Case
-
-  test "game is over with an empty list" do
-    assert evaluate([]) == true
-  end
+  alias TicTacToe.Board
 
   test "game is not over with an empty board" do
-    assert evaluate([
-      0, 1, 2,
-      3, 4, 5,
-      6, 7, 8]) == false
+    assert evaluate(%Board{}) == false
   end
 
   test "full board but no winner" do
-    assert evaluate([
+    assert evaluate(%Board{cells: [
       "X","O","X",
       "O","X","O",
-      "O","X","O"]) == false
+      "O","X","O"]}) == false
   end
 
   test "X is the winner" do
-    assert evaluate([
+    assert evaluate(%Board{cells: [
       "X","O","X",
       "O","X","O",
-       6,  7 ,"X"]) == true
+       6,  7 ,"X"]}) == true
   end
 
   test "O is the winner" do
-    assert evaluate([
+    assert evaluate(%Board{cells: [
       "X","X","O",
       "X","O","X",
-      "O", 7,  8]) == true
+      "O", 7,  8]}) == true
   end
 
   defp evaluate(board) do
