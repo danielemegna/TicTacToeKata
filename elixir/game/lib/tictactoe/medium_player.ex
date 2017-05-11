fe65b67464451212e3491a3f628b2c6b378509a2
Daniele Megna <daniele.megna@xpeppers.com>
2017-05-11 09:23:49 +0200

    Integrating Board module in medium_player

--- elixir/game/lib/tictactoe/medium_player.ex
+++ elixir/game/lib/tictactoe/medium_player.ex
@@ -1,9 +1,10 @@
 defmodule TicTacToe.MediumPlayer do
   alias TicTacToe.Referee
+  alias TicTacToe.Board
 
   def next_move(board) do
     cond do
-      center_move_available?(board) -> 4
+      Board.free?(%Board{cells: board}, 4) -> 4
       true -> evaluate(board)
     end
   end
@@ -29,8 +30,8 @@ defmodule TicTacToe.MediumPlayer do
 
   defp get_best_move([first_index|rest], board) do
     cond do
-      Referee.game_over?(List.update_at(board, first_index, fn(_) -> "O" end)) -> first_index
-      Referee.game_over?(List.update_at(board, first_index, fn(_) -> "X" end)) -> first_index
+      Referee.game_over?(%Board{cells: List.update_at(board, first_index, fn(_) -> "O" end)}) -> first_index
+      Referee.game_over?(%Board{cells: List.update_at(board, first_index, fn(_) -> "X" end)}) -> first_index
       true -> get_best_move(rest, board)
     end
   end
