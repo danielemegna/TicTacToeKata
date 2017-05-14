986b8dc98fc7b6fa830404a56cb5befbe6d4e34e
Daniele Megna <megna.dany@gmail.com>
2017-05-14 12:09:24 +0200

    Added play util function in Player module

--- elixir/game/lib/tictactoe/player/player.ex
+++ elixir/game/lib/tictactoe/player/player.ex
@@ -1,4 +1,14 @@
 defmodule TicTacToe.Player do
+  alias TicTacToe.Board
+
   @enforce_keys [:sign, :strategy]
   defstruct @enforce_keys
+
+  def play(board, player) do
+    board |> Board.mark(
+      player.strategy.next_move(board),
+      player.sign
+    )
+  end
+
 end
