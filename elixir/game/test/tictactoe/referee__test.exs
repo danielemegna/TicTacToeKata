a50578b16366ffb6ccd96070dc5de9d2b520c6bd
Daniele Megna <megna.dany@gmail.com>
2017-05-15 07:56:05 +0200

    Renamed Referee test module
    Now it refers to Referee module and not a specific Referee function

--- elixir/game/test/tictactoe/referee__test.exs
+++ elixir/game/test/tictactoe/referee__test.exs
@@ -1,40 +1,40 @@
-defmodule TicTacToe.Referee_GameOver_Test do
+defmodule TicTacToe.Referee_Test do
   use ExUnit.Case
   alias TicTacToe.Board
 
   test "game is not over with an empty board" do
-    assert evaluate(%Board{}) == false
+    assert evaluate_game_over(%Board{}) == false
   end
 
   test "full board but no winner" do
-    assert evaluate(%Board{cells: [
+    assert evaluate_game_over(%Board{cells: [
       "X","O","X",
       "O","X","O",
       "O","X","O"]}) == true
   end
 
   test "X is the winner" do
-    assert evaluate(%Board{cells: [
+    assert evaluate_game_over(%Board{cells: [
       "X","O","X",
       "O","X","O",
        6,  7 ,"X"]}) == true
   end
 
   test "O is the winner" do
-    assert evaluate(%Board{cells: [
+    assert evaluate_game_over(%Board{cells: [
       "X","X","O",
       "X","O","X",
       "O", 7,  8]}) == true
   end
 
   test "no winner and free spaces!" do
-    assert evaluate(%Board{cells: [
+    assert evaluate_game_over(%Board{cells: [
       "X",1,"O",
        3,"O",5,
        6,"X",8]}) == false
   end
 
-  defp evaluate(board) do
+  defp evaluate_game_over(board) do
     TicTacToe.Referee.game_over? board
   end
 
