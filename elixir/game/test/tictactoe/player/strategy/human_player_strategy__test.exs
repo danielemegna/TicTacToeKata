093e8c752be1c46ddf0a888458494fd602f42558
Daniele Megna <megna.dany@gmail.com>
2017-05-16 07:57:14 +0200

    Refactoring in Strategy.Human_Test
    Added assert on std output

--- elixir/game/test/tictactoe/player/strategy/human_player_strategy__test.exs
+++ elixir/game/test/tictactoe/player/strategy/human_player_strategy__test.exs
@@ -5,18 +5,28 @@ defmodule TicTacToe.Player.Strategy.Human_Test do
   alias TicTacToe.Player
 
   test "mark first cell" do
+    board = %Board{}
     user_choice = "0"
-    next_move(%Board{}, user_choice)
+
+    assert next_move(board, user_choice) == "Enter [0-8]>"
     assert_received {:chosen_move, 0}
   end
 
   test "trying to mark a marked cell, player can choose another one" do
-    user_choices = "0\n" <> "3\n" <> "4"
-    next_move(%Board{cells: [
+    board = %Board{cells: [
       "O", 1 , 2,
       "X", 4 , 5,
        6 , 7 , 8
-    ]}, user_choices)
+    ]}
+    user_choices =
+      "0\n"<>
+      "3\n"<>
+      "4"
+
+    assert next_move(board, user_choices) ==
+      "Enter [0-8]>"<>
+      "Enter [0-8]>"<>
+      "Enter [0-8]>"
     assert_received {:chosen_move, 4}
   end
 
