c0fd4cd088c55892f246948dd7d27f3ca9c981b8
Daniele Megna <megna.dany@gmail.com>
2017-05-18 07:46:10 +0200

    Refactoring in Player.Strategy.Human module

--- elixir/game/lib/tictactoe/player/strategy/human.ex
+++ elixir/game/lib/tictactoe/player/strategy/human.ex
@@ -2,28 +2,24 @@ defmodule TicTacToe.Player.Strategy.Human do
   alias TicTacToe.Board
 
   def next_move(board) do
-    IO.write("Enter [0-8]")
-    Integer.parse(IO.gets(">"))
+    "Enter [0-8]>"
+      |> IO.gets
+      |> Integer.parse
       |> evaluate(board)
   end
 
-  defp evaluate({move, _}, board) do
-  
-    cond do
-      (move < 0 || move > 8) ->
-        IO.puts("Bad input! Retry..")
-        next_move(board)
-      Board.free?(board, move) ->
-        move
-      true ->
-        IO.puts("Cell #{move} already marked! Retry..")
-        next_move(board)
+  defp evaluate({move, _}, board) when move >= 0 and move <= 8 do
+    unless Board.free?(board, move) do
+      "Cell #{move} already marked! Retry.." |> IO.puts
+      next_move(board)
+    else
+      move
     end
   end
 
-  defp evaluate(:error, board) do
-      IO.puts("Bad input! Retry..")
-      next_move(board)
+  defp evaluate(_, board) do
+    "Bad input! Retry.." |> IO.puts
+    next_move(board)
   end
 
 end
