442d540041a5f486e3b3469740d94f77fb4fb247
Daniele Megna <daniele.megna@xpeppers.com>
2017-05-19 14:38:20 +0200

    Began Minimax module developments

---
+++ elixir/game/lib/tictactoe/player/strategy/minimax.ex
@@ -0,0 +1,7 @@
+defmodule TicTacToe.Player.Strategy.Minimax do
+
+  def value(_,_,_) do
+    0
+  end
+
+end
---
+++ elixir/game/test/tictactoe/player/strategy/minimax__test.exs
@@ -0,0 +1,27 @@
+defmodule TicTacToe.Player.Strategy.Minimax_Test do
+  use ExUnit.Case
+  alias TicTacToe.Board
+  alias TicTacToe.Player.Strategy.Minimax
+
+  test 'last tie move worth zero' do
+    board = %Board{cells: [
+      "X","O","X",
+      "O","X", 5 ,
+      "O","X","O"
+    ]}
+
+    assert Minimax.value(5, "O", board) == 0
+  end
+
+  @tag :skip
+  test 'win move worth two' do
+    board = %Board{cells: [
+      "X","O", 2 ,
+      "O","X","O",
+      "O","X","O"
+    ]}
+
+    assert Minimax.value(2, "O", board) == 2
+  end
+
+end
