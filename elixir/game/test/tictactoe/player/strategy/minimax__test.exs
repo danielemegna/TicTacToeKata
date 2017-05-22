bd8989bfe226d0f3f515fde05e225a8217b91305
Daniele Megna <megna.dany@gmail.com>
2017-05-22 07:44:17 +0200

    Covered minimax use cases in Minimax module test

--- elixir/game/test/tictactoe/player/strategy/minimax__test.exs
+++ elixir/game/test/tictactoe/player/strategy/minimax__test.exs
@@ -11,7 +11,7 @@ defmodule TicTacToe.Player.Strategy.Minimax_Test do
     ], 5) == 0
   end
 
-  test 'win move worth two' do
+  test 'one-move win worth two' do
     assert evaluate([
        0 ,"O","X",
       "O","X","O",
@@ -19,12 +19,60 @@ defmodule TicTacToe.Player.Strategy.Minimax_Test do
     ], 0) == 2
   end
   
-  test 'let opponent win at next move worth minus one' do
+  test 'let opponent win worth minus one' do
     assert evaluate([
        0 , 1 ,"O",
       "X","X","O",
       "X","O","X"
     ], 1) == -1
+  end
+
+  test 'any move value zero with no chance of win' do
+    board = [
+       0 , 1 ,"X",
+      "X","O","O",
+       6 , 7 ,"X"
+    ]
+
+    assert evaluate(board, 0) == 0
+    assert evaluate(board, 1) == 0
+    assert evaluate(board, 6) == 0
+    assert evaluate(board, 7) == 0
+  end
+
+  test 'any move that lead to defeat worth -1' do
+    board = [
+      "X", 1 , 2 ,
+       3 , 4 , 5 ,
+       6 , 7 , 8 
+    ]
+
+    assert evaluate(board, 4) == 0
+    assert evaluate(board, 2) == -1
+    assert evaluate(board, 3) == -1
+    assert evaluate(board, 5) == -1
+    assert evaluate(board, 8) == -1
+  end
+
+  test 'moves worth 1 when lead to sure win' do
+    board = [
+       0 , 1 ,"X",
+       3 , 4 , 5 ,
+       6 , 7 ,"O"
+    ]
+
+    # win moves
+    assert evaluate(board, 0) == 1
+    assert evaluate(board, 6) == 1
+    assert evaluate(board, 7) == 1
+
+    # tie moves
+    assert evaluate(board, 1) == 0
+    assert evaluate(board, 3) == 0
+    assert evaluate(board, 4) == 0
+
+    # defeat move
+    assert evaluate(board, 5) == -1
   end
 
   defp evaluate(cells, move) do
