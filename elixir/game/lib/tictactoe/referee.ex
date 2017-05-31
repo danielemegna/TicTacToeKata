0657bd4d7a186a318a6fe8f74156b48d2ccece52
Daniele Megna <megna.dany@gmail.com>
2017-05-31 08:20:44 +0200

    Refactored Referee winner function

--- elixir/game/lib/tictactoe/referee.ex
+++ elixir/game/lib/tictactoe/referee.ex
@@ -20,15 +20,29 @@ defmodule TicTacToe.Referee do
       [0, 4, 8], [2, 4, 6]
     ]
 
-    winner = win_combinations
-      |> Enum.map(fn(combination) -> combination
-        |> Enum.map(&(Board.at(board, &1)))
-        |> Enum.uniq
-      end)
+    win_combinations
+      |> signs_in_combinations(board)
+      |> keep_only_winner_signs
+      |> winner_sign
+  end
+
+  defp keep_only_winner_signs(signs_in_combinations) do
+    signs_in_combinations
       |> Enum.filter(&(Enum.count(&1) == 1))
       |> List.flatten
+  end
 
-    List.first(winner ++ [:none])
+  defp winner_sign([]), do: :none
+  defp winner_sign([winner|_]), do: winner
+
+  defp signs_in_combinations(combinations, board) do
+    Enum.map(combinations, &(signs_in_combination(&1, board)))
+  end
+
+  defp signs_in_combination(combination, board) do
+    combination
+      |> Enum.map(&(Board.at(board, &1)))
+      |> Enum.uniq
   end
 
   defp tie?(board) do
