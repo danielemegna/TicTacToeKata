d6eaef38644ee38268012114eebf4fe43a4c4a2b
Daniele Megna <megna.dany@gmail.com>
2017-06-01 08:14:33 +0200

    Refactored Game smoke tests

--- elixir/game/test/game__test.exs
+++ elixir/game/test/game__test.exs
@@ -4,7 +4,8 @@ defmodule Game_Test do
   import ExUnit.CaptureIO
 
   test "fast game over smoke test" do
-    output = game("0\n1\n3\n")
+    user_inputs = [0,1,3]
+    output = game(user_inputs)
 
     assert_contains(output, "Enter [0-8]>")
     assert_contains(output, "X")
@@ -12,7 +13,8 @@ defmodule Game_Test do
   end
 
   test "tie game smoke test" do
-    output = game("4\n2\n3\n1\n8")
+    user_inputs = [4,2,3,1,8]
+    output = game(user_inputs)
 
     assert_contains(output, "X")
     assert_contains(output, "O")
@@ -20,7 +22,8 @@ defmodule Game_Test do
   end
 
   test "wrong inputs smoke test" do
-    output = game("bad\n9\n0\n-1\n4\n1\n2\n5")
+    user_inputs = ["bad",0,4,1,2,5]
+    output = game(user_inputs)
 
     assert_contains(output, "Bad input! Retry..")
     assert_contains(output, "Cell 4 already marked! Retry..")
@@ -28,7 +31,8 @@ defmodule Game_Test do
     assert_contains(output, "O wins! Game Over")
   end
 
-  defp game(input) do
+  defp game(user_inputs) do
+    input = Enum.join(user_inputs, "\n")
     capture_io(input, fn ->
       assert Game.start_game == :ok
     end)
