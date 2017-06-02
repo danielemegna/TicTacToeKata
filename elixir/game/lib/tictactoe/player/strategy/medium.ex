8f90cf2b15ae929ba2c4a06de0ab1126e3ac1617
Daniele Megna <megna.dany@gmail.com>
2017-06-02 15:21:54 +0200

    Refactoring in Medium player strategy

--- elixir/game/lib/tictactoe/player/strategy/medium.ex
+++ elixir/game/lib/tictactoe/player/strategy/medium.ex
@@ -2,39 +2,24 @@ defmodule TicTacToe.Player.Strategy.Medium do
   alias TicTacToe.Referee
   alias TicTacToe.Board
 
-  def next_move(board, my_sign) do
+  def next_move(board, _) do
     board 
       |> Board.available_moves
-      |> get_best_move(board, my_sign)
+      |> get_best_move(board)
   end
 
-  defp get_best_move([move|[]], _, _) do
-    move
+  defp get_best_move([4|_], _), do: 4
+  defp get_best_move([move|[]], _), do: move
+
+  defp get_best_move([move|rest], board) do
+    if can_game_over?(board, move), do: move,
+    else: get_best_move(rest, board)
   end
 
-  defp get_best_move([4|_], _, _) do
-    4
-  end
-
-  defp get_best_move([move|rest], board, my_sign) do
-    can_game_over? = 
-      Referee.game_over?(
-        board |> Board.mark(move, my_sign)
-      ) == {:yes, my_sign}
-      ||
-      Referee.game_over?(
-        board |> Board.mark(move, opponent(my_sign))
-      ) == {:yes, opponent(my_sign)}
-  
-    if can_game_over? do
-      move
-    else
-      get_best_move(rest, board, my_sign)
-    end
-  end
-
-  defp opponent(sign) do
-    case sign, do: ("X"->"O"; _->"X")
+  defp can_game_over?(board, move) do
+    Board.mark(board, move, "X") |> Referee.game_over? == {:yes, "X"}
+    or
+    Board.mark(board, move, "O") |> Referee.game_over? == {:yes, "O"}
   end
 
 end
