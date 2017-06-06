cc72776351642a631d1a276214b7876287c1e250
Daniele Megna <daniele.megna@xpeppers.com>
2017-06-06 09:25:41 +0200

    Added menu to choose the difficulty write

--- elixir/game/lib/tictactoe/player/player_factory.ex
+++ elixir/game/lib/tictactoe/player/player_factory.ex
@@ -2,6 +2,14 @@ defmodule TicTacToe.PlayerFactory do
   alias TicTacToe.Player
 
   def pair do
+    "Choose computer level of difficulty\n"<>
+      " 1. Easy\n"<>
+      " 2. Medium\n"<>
+      " 3. Hard (unbeatable)\n"<>
+      "Enter [1-3]>" |> IO.write
+
+    "Hard computer player choosen" |> IO.puts
+
     [
       %Player{ sign: "X", strategy: Player.Strategy.Human },
       %Player{ sign: "O", strategy: Player.Strategy.Hard },
--- elixir/game/test/tictactoe/player/player_factory__test.exs
+++ elixir/game/test/tictactoe/player/player_factory__test.exs
@@ -1,13 +1,29 @@
 defmodule TicTacToe.PlayerFactory_Test do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   alias TicTacToe.Player
   alias TicTacToe.PlayerFactory
 
   test 'pair returns human and hard players' do
-    assert PlayerFactory.pair == [
-      %Player{ sign: "X", strategy: Player.Strategy.Human },
-      %Player{ sign: "O", strategy: Player.Strategy.Hard },
-    ]
+    capture_io(fn ->
+      assert PlayerFactory.pair == [
+        %Player{ sign: "X", strategy: Player.Strategy.Human },
+        %Player{ sign: "O", strategy: Player.Strategy.Hard },
+      ]
+    end)
+  end
+
+  test 'pair writes out menu to choose the difficulty' do
+    output = capture_io(fn ->
+      PlayerFactory.pair
+    end)
+
+    assert output == "Choose computer level of difficulty\n"<>
+      " 1. Easy\n"<>
+      " 2. Medium\n"<>
+      " 3. Hard (unbeatable)\n"<>
+      "Enter [1-3]>"<>
+      "Hard computer player choosen\n"
   end
 
 end
