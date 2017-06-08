8b3b0c36538a5babc86a9be0f070e2bb51061ed8
Daniele Megna <daniele.megna@xpeppers.com>
2017-06-08 08:27:16 +0200

    Developed player difficulty choose in PlayerFactory
    Now it supports Medium and Hard computer player

--- elixir/game/lib/tictactoe/player/player_factory.ex
+++ elixir/game/lib/tictactoe/player/player_factory.ex
@@ -2,18 +2,32 @@ defmodule TicTacToe.PlayerFactory do
   alias TicTacToe.Player
 
   def pair do
-    "Choose computer level of difficulty\n"<>
-      " 1. Easy\n"<>
-      " 2. Medium\n"<>
-      " 3. Hard (unbeatable)\n"<>
-      "Enter [1-3]>" |> IO.write
-
-    "Hard computer player choosen" |> IO.puts
-
     [
       %Player{ sign: "X", strategy: Player.Strategy.Human },
-      %Player{ sign: "O", strategy: Player.Strategy.Hard },
+      choose_computer_player(),
     ]
+  end
+
+  defp choose_computer_player do
+    "Choose computer level of difficulty\n"<>
+    " 1. Easy\n"<>
+    " 2. Medium\n"<>
+    " 3. Hard (unbeatable)\n" <>
+    "Enter [1-3]>"
+      |> IO.gets
+      |> Integer.parse
+      |> computer_player_for
+  end
+
+  defp computer_player_for({type, _}) when type >= 1 and type <= 3 do
+    %Player{ sign: "O", strategy: computer_strategy_for(type) }
+  end
+
+  defp computer_strategy_for(type) do
+    case type do
+      2 -> Player.Strategy.Medium
+      _ -> Player.Strategy.Hard
+    end
   end
 
 end
--- elixir/game/test/tictactoe/player/player_factory__test.exs
+++ elixir/game/test/tictactoe/player/player_factory__test.exs
@@ -4,26 +4,40 @@ defmodule TicTacToe.PlayerFactory_Test do
   alias TicTacToe.Player
   alias TicTacToe.PlayerFactory
 
-  test 'pair returns human and hard players' do
-    capture_io(fn ->
-      assert PlayerFactory.pair == [
-        %Player{ sign: "X", strategy: Player.Strategy.Human },
-        %Player{ sign: "O", strategy: Player.Strategy.Hard },
-      ]
-    end)
-  end
-
   test 'pair writes out menu to choose the difficulty' do
-    output = capture_io(fn ->
+    output = capture_io("3", fn ->
       PlayerFactory.pair
     end)
 
-    assert output == "Choose computer level of difficulty\n"<>
+    assert output ==
+      "Choose computer level of difficulty\n"<>
       " 1. Easy\n"<>
       " 2. Medium\n"<>
       " 3. Hard (unbeatable)\n"<>
-      "Enter [1-3]>"<>
-      "Hard computer player choosen\n"
+      "Enter [1-3]>"
+  end
+
+  test 'hard player choosen' do
+    assert_pair(3, [
+      %Player{ sign: "X", strategy: Player.Strategy.Human },
+      %Player{ sign: "O", strategy: Player.Strategy.Hard },
+    ])
+  end
+
+  test 'medium player choosen' do
+    assert_pair(2, [
+      %Player{ sign: "X", strategy: Player.Strategy.Human },
+      %Player{ sign: "O", strategy: Player.Strategy.Medium },
+    ])
+  end
+
+  defp assert_pair(integer, expected) when is_integer(integer), do:
+    assert_pair(Integer.to_string(integer), expected)
+
+  defp assert_pair(input, expected) do
+    capture_io(input, fn ->
+      assert PlayerFactory.pair == expected
+    end)
   end
 
 end
