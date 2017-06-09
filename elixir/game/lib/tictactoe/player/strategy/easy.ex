8b8b46df64b20731b74796880c0816352dc92fe0
Daniele Megna <megna.dany@gmail.com>
2017-06-09 08:07:53 +0200

    Developed Player.Strategy.Easy module

---
+++ elixir/game/lib/tictactoe/player/strategy/easy.ex
@@ -0,0 +1,23 @@
+defmodule TicTacToe.Player.Strategy.Easy do
+  alias TicTacToe.Referee
+  alias TicTacToe.Board
+
+  def next_move(board, _) do
+    board 
+      |> Board.available_moves
+      |> get_best_move(board)
+  end
+
+  defp get_best_move([move|[]], _), do: move
+  defp get_best_move([move|rest], board) do
+    if !can_game_over?(board, move), do: move,
+    else: get_best_move(rest, board)
+  end
+
+  defp can_game_over?(board, move) do
+    Board.mark(board, move, "X") |> Referee.game_over? == {:yes, "X"}
+    or
+    Board.mark(board, move, "O") |> Referee.game_over? == {:yes, "O"}
+  end
+
+end
---
+++ elixir/game/test/tictactoe/player/strategy/easy_player_strategy__test.exs
@@ -0,0 +1,67 @@
+defmodule TicTacToe.Player.Strategy.Easy_Test do
+  use ExUnit.Case
+  alias TicTacToe.Board
+  alias TicTacToe.Player
+
+  test "chooses first no gameover move" do
+    assert next_move([
+       0 , 1,  2,
+       3 , 4,  5,
+       6 , 7,  8
+    ]) == 0
+    assert next_move([
+       0 , 1,  2,
+       3 ,"X", 5,
+       6 , 7,  8
+    ]) == 0
+    assert next_move([
+      "O", 1 , 2,
+       3 ,"X", 5,
+       6 , 7 , 8
+    ]) == 1
+  end
+
+  test "avoids opponent win moves" do
+    assert next_move([
+      "O", 1 , 2,
+       3 ,"X", 5,
+       6 ,"X", 8
+    ]) == 2
+    assert next_move([
+      "O", 1 , 2,
+       3 ,"X","O",
+      "X","X", 8
+    ]) == 3
+  end
+
+  test "avoid also win moves!" do
+    assert next_move([
+      "O","O", 2,
+       3 ,"X", 5,
+       6 , 7 ,"X"
+    ]) == 3
+    assert next_move([
+      "X","O", 2,
+       3 ,"O", 5,
+       6 , 7, "X"
+    ]) == 2
+    assert next_move([
+      "X", 1 , 2,
+       3 ,"O","X",
+       6 ,"O","X"
+    ]) == 3
+  end
+
+  test "wins only when compelled" do
+    assert next_move([
+      "O","O", 2,
+      "O","X","X",
+       6 ,"X","X"
+    ]) == 6
+  end
+
+  defp next_move(board) do
+    Player.Strategy.Easy.next_move(%Board{cells: board}, "O")
+  end
+
+end
