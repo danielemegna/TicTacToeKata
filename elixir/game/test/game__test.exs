20c17205c7d50cb3dd716304d871e6be769f2756
Daniele Megna <megna.dany@gmail.com>
2017-06-14 08:27:58 +0200

    Added human_vs_human tie game smoke test

--- elixir/game/test/game__test.exs
+++ elixir/game/test/game__test.exs
@@ -2,6 +2,7 @@ defmodule Game_Test do
   use ExUnit.Case
   import ExUnit.CaptureIO
 
+  @human_vs_human 1
   @human_vs_computer 3
   
   @easy_computer_player 1
@@ -68,6 +69,17 @@ defmodule Game_Test do
     assert_contains(output, "X wins! Game Over")
   end
 
+  test "human vs human tie game smoke test" do
+    players_choice = [@human_vs_human]
+    game_moves = [4,0,2,6,3,5,1,7,8]
+    user_inputs = players_choice ++ game_moves
+
+    output = game(user_inputs)
+
+    assert_not_contains(output, "Choose computer level of difficulty")
+    assert_contains(output, "Tie! Game Over")
+  end
+
   defp game(user_inputs) do
     input = Enum.join(user_inputs, "\n")
     capture_io(input, fn ->
@@ -78,6 +90,10 @@ defmodule Game_Test do
   defp assert_contains(string, substring) do
     assert String.contains?(string, substring),
       "Expected #{inspect string} to contain #{inspect substring}"
+  end
+  defp assert_not_contains(string, substring) do
+    assert !String.contains?(string, substring),
+      "Expected #{inspect string} to NOT contain #{inspect substring}"
   end
 
 end
