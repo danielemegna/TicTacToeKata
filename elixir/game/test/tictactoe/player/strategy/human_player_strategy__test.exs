c3b90fd96952fcdfc4ab38dcd68ad96eb174b4ee
Daniele Megna <megna.dany@gmail.com>
2017-06-24 20:10:39 +0200

    Improved Strategy.Human tests

--- elixir/game/test/tictactoe/player/strategy/human_player_strategy__test.exs
+++ elixir/game/test/tictactoe/player/strategy/human_player_strategy__test.exs
@@ -7,32 +7,38 @@ defmodule TicTacToe.Player.Strategy.Human_Test do
   test "mark first cell" do
     board = Board.new
     user_choice = "0"
+    expected = 0
 
-    assert next_move(board, user_choice) ==
+    output = next_move(board, user_choice, expected)
+
+    assert output ==
       "'X' player, choose your next move!\n"<>
       "Enter [0-8]>"
-
-    assert_received {:chosen_move, 0}
   end
 
   test "bad user input" do
     board = Board.new
     user_choices = "bad\n" <> "6"
+    expected = 6
 
-    assert next_move(board, user_choices) ==
+    output = next_move(board, user_choices, expected)
+
+    assert output ==
       "'X' player, choose your next move!\n"<>
       "Enter [0-8]>"<>
       "Bad input! Retry..\n\n"<>
       "'X' player, choose your next move!\n"<>
       "Enter [0-8]>"
-    assert_received {:chosen_move, 6}
   end
 
   test "move index out of board" do
     board = Board.new
     user_choices = "9\n" <> "11\n" <> "-1\n" <> "5"
+    expected = 5
 
-    assert next_move(board, user_choices) ==
+    output = next_move(board, user_choices, expected)
+
+    assert output ==
       "'X' player, choose your next move!\n"<>
       "Enter [0-8]>"<>
       "Bad input! Retry..\n\n"<>
@@ -44,7 +50,6 @@ defmodule TicTacToe.Player.Strategy.Human_Test do
       "Bad input! Retry..\n\n"<>
       "'X' player, choose your next move!\n"<>
       "Enter [0-8]>"
-    assert_received {:chosen_move, 5}
   end
 
   test "trying to mark a marked cell, player can choose another one" do
@@ -53,12 +58,12 @@ defmodule TicTacToe.Player.Strategy.Human_Test do
       "X", 4 , 5,
        6 , 7 , 8
     ]}
-    user_choices =
-      "0\n"<>
-      "3\n"<>
-      "4"
+    user_choices = "0\n"<> "3\n"<> "4"
+    expected = 4
 
-    assert next_move(board, user_choices) ==
+    output = next_move(board, user_choices, expected)
+
+    assert output ==
       "'X' player, choose your next move!\n"<>
       "Enter [0-8]>"<>
       "Cell 0 already marked! Retry..\n\n"<>
@@ -67,13 +72,12 @@ defmodule TicTacToe.Player.Strategy.Human_Test do
       "Cell 3 already marked! Retry..\n\n"<>
       "'X' player, choose your next move!\n"<>
       "Enter [0-8]>"
-    assert_received {:chosen_move, 4}
   end
 
-  defp next_move(board, input) do
+  defp next_move(board, input, expected_move) do
     capture_io(input, fn ->
       chosen_move = Player.Strategy.Human.next_move(board, "X")
-      send self(), {:chosen_move, chosen_move}
+      assert expected_move == chosen_move
     end)
   end
 end
