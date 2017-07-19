16d16fc37209ac212043180d980e211386c0c8ee
Daniele Megna <daniele.megna@xpeppers.com>
2017-07-19 08:57:09 +0200

    Removed IO from PlayerFactory
    Moved IO interactions from PlayerFactory into new module IOAdapter

---
+++ elixir/game/lib/tictactoe/ioadapter.ex
@@ -0,0 +1,28 @@
+defmodule TicTacToe.IOAdapter do
+
+  def game_type? do
+    "Choose game type\n"<>
+    " 1. Human v. Human\n"<>
+    " 2. Human v. Computer\n"<>
+    " 3. Computer v. Human\n"<>
+    " 4. Computer v. Computer\n"<>
+    "Enter [1-4]>"
+      |> IO.gets
+      |> Integer.parse
+  end
+
+  def bad_input do
+    "Bad input! Retry..\n" |> IO.puts
+  end
+
+  def difficulty_level?(sign) do
+    "Choose computer level of difficulty ('#{sign}' symbol player)\n"<>
+    " 1. Easy\n"<>
+    " 2. Medium\n"<>
+    " 3. Hard (unbeatable)\n" <>
+    "Enter [1-3]>"
+      |> IO.gets
+      |> Integer.parse
+  end
+
+end
--- elixir/game/lib/tictactoe/player/player_factory.ex
+++ elixir/game/lib/tictactoe/player/player_factory.ex
@@ -1,26 +1,14 @@
 defmodule TicTacToe.PlayerFactory do
   alias TicTacToe.Player
+  alias TicTacToe.IOAdapter
 
-  def pair do
-    choose_game_type()
-      |> pair_for_game_type
-  end
+  def pair, do:
+    choose_game_type() |> pair_for_game_type
 
-  defp choose_game_type do
-    "Choose game type\n"<>
-    " 1. Human v. Human\n"<>
-    " 2. Human v. Computer\n"<>
-    " 3. Computer v. Human\n"<>
-    " 4. Computer v. Computer\n"<>
-    "Enter [1-4]>"
-      |> IO.gets
-      |> Integer.parse
-      |> game_type_for
-  end
-
+  defp choose_game_type, do: IOAdapter.game_type? |> game_type_for
   defp game_type_for({type, _}) when type in 1..4, do: type
   defp game_type_for(_) do
-    "Bad input! Retry..\n" |> IO.puts
+    IOAdapter.bad_input
     choose_game_type()
   end
 
@@ -36,23 +24,14 @@ defmodule TicTacToe.PlayerFactory do
   defp human_player(sign), do:
     %Player{ sign: sign, strategy: Player.Strategy.Human }
 
-  defp computer_player(sign) do
-    "Choose computer level of difficulty ('#{sign}' symbol player)\n"<>
-    " 1. Easy\n"<>
-    " 2. Medium\n"<>
-    " 3. Hard (unbeatable)\n" <>
-    "Enter [1-3]>"
-      |> IO.gets
-      |> Integer.parse
-      |> computer_player_for(sign)
-  end
+  defp computer_player(sign), do:
+      IOAdapter.difficulty_level?(sign) |> computer_player_for(sign)
 
-  defp computer_player_for({type, _},sign) when type in 1..3 do
+  defp computer_player_for({type, _},sign) when type in 1..3, do:
     %Player{ sign: sign, strategy: computer_strategy_for(type) }
-  end
 
   defp computer_player_for(_,sign) do
-    "Bad input! Retry..\n" |> IO.puts
+    IOAdapter.bad_input
     computer_player(sign)
   end
 
