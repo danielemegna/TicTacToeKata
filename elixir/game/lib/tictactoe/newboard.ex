7fccf471c973bdff12b7f34c0703c297e48f6455
Daniele Megna <daniele.megna@xpeppers.com>
2017-07-24 11:10:22 +0200

    Developing NewBoard module

--- elixir/game/lib/tictactoe/newboard.ex
+++ elixir/game/lib/tictactoe/newboard.ex
@@ -1,8 +1,19 @@
 defmodule TicTacToe.NewBoard do
+  alias :math, as: Math
   defstruct size: 3, occupied: %{}
 
   def new do
     %__MODULE__{}
+  end
+
+  def new(values) do
+    size = Math.sqrt(Enum.count(values))
+    occupied = values
+      |> Enum.with_index()
+      |> Enum.map(fn {v,i}->{i,v} end)
+      |> Map.new
+
+    %__MODULE__{size: size, occupied: occupied}
   end
 
   def free?(board, index) do
@@ -12,6 +23,15 @@ defmodule TicTacToe.NewBoard do
   def mark(board, index, sign) do
     new_occupied = Map.put(board.occupied, index, sign)
     %__MODULE__{size: board.size, occupied: new_occupied}
+  end
+
+  def at(board, index) do
+    if free?(board, index), do: :empty,
+    else: board.occupied[index]
+  end
+
+  def full?(board) do
+    Enum.count(Map.keys(board.occupied)) == (board.size*board.size)
   end
 
 end
--- elixir/game/test/tictactoe/newboard__test.exs
+++ elixir/game/test/tictactoe/newboard__test.exs
@@ -22,4 +22,27 @@ defmodule TicTacToe.NewBoard_Test do
     assert Board.free?(board, 1)
   end
 
+  test 'get value at index' do
+    board = Board.new
+    assert Board.at(board, 1) == :empty
+    assert Board.at(board, 4) == :empty
+
+    board = Board.mark(board, 1, "X")
+    assert Board.at(board, 1) == "X" 
+    assert Board.at(board, 4) == :empty
+  end
+
+  test 'empty board is not full' do
+    assert Board.full?(Board.new) == false
+  end
+
+  test 'full board should.. be full' do
+    board = Board.new [
+      "X","O","X",
+      "O","X","O",
+      "O","X","O"]
+
+    assert Board.full?(board) == true
+  end
+
 end
